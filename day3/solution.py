2f3b624a03beb9912c61a18320815aa04b488593
Spike Leung <l-yanlei@hotmail.com>
2025-12-03 14:30:21 +0800

    finish day3-part2

--- day3/solution.py
+++ day3/solution.py
@@ -14,3 +14,23 @@ with open("input.txt", 'r', encoding="utf-8") as file:
       total += int(b + sorted(list(line[bIndex+1:]), reverse=True)[0])
 
   print(f'part1 result: {total}')
+
+def findBestNumber(str, count):
+  if count == 0:
+    return ''
+
+  start = 0
+  end = len(str) - count + 1
+  searchStr = str[start:end]
+  bestNumber = sorted(list(searchStr), reverse=True)[0]
+  bestNumberIndex = str.find(bestNumber)
+
+  return bestNumber + findBestNumber(str[bestNumberIndex+1:], count - 1)
+
+#part2
+with open("input.txt", 'r', encoding="utf-8") as file:
+  total = 0
+  for line in file:
+    line = line.strip()
+    total += int(findBestNumber(line, 12))
+  print(f'part2 result: {total}')
